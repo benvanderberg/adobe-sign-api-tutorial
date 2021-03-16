37979b960e339846345df253ed41990678afdd9c
Travis Krause <tkrause@adobe.com>
2021-03-15 21:38:01 -0500

    tweaks

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # adobe-sign-api-tutorial
+
+## Dev
+
+To run locally, you'll need:
+
+- python 3.9+
+- pipenv
+
+```bash
+git clone https://github.com/benvanderberg/adobe-sign-api-tutorial.git
+cd adobe-sign-api-tutorial
+pipenv install
+pipenv run serve
+```
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import time
+from functools import cache
 import requests
 from flask import Flask, render_template, request, url_for
 from dotenv import load_dotenv
@@ -9,6 +11,11 @@ app = Flask(__name__)
 
 
 def sign_validate(resp, code=200):
+    """
+    An overly simplistic validator for Sign API responses.
+
+    In a production environment, you'll want this to be MUCH more sophisticated.
+    """
     if resp.status_code != code:
         raise Exception(
             {
@@ -19,12 +26,23 @@ def sign_validate(resp, code=200):
         )
 
 
+@cache
 def headers():
+    """
+    Basic helper for generating headers w/ a valid integration key embedded.
+    """
+    #  Grab the integration key from the env
     token = os.getenv("INTEGRATION_KEY")
     return {"Authorization": f"Bearer {token}"}
 
 
+@cache
 def base():
+    """
+    Use the baseUris call to retrieve the api access point.
+
+    In practice, with an integration key, this value can be stored as an env.
+    """
     # Send request
     url = "https://api.na1.echosign.com:443/api/rest/v6/baseUris"
     resp = requests.get(url, headers=headers())
@@ -60,6 +78,15 @@ def submitted():
 
 @app.route("/webform")
 def webform_index():
+    """
+    Send request to page with a widget on it.
+
+    We're including the values in a query string, so that we may generate an
+    iframe the pre-populates form fields.
+
+    Since the webform requires an email to be user entered, we're removing it
+    from the form.
+    """
     return render_template(
         "index.html", action="/webform/sign", method="GET", email=False
     )
@@ -67,10 +94,17 @@ def webform_index():
 
 @app.route("/webform/sign", methods=["GET"])
 def webform_sign():
+    """
+    Embed a web form into an iframe.
+
+    We are also taking the query string and interpolating it into the webform
+    url in such a way that it'll pre-populate data.
+    """
     url = os.getenv("WAIVER_WEBFORM")
     app.logger.debug(url)
 
     # Build webform URL
+    # https://{base}/public/esignWidget?wid={id}#{field1}={v1}&{field2}={v2}
     data = dict(request.args)
     if data:
         params = []
@@ -90,6 +124,9 @@ def webform_sign():
 
 @app.route("/send")
 def send_index():
+    """
+    Nothing fancy here, just a form that sends post data.
+    """
     return render_template(
         "index.html", action="/send/submitted", method="POST", email=True
     )
@@ -97,6 +134,9 @@ def send_index():
 
 @app.route("/send/submitted", methods=["POST"])
 def send_submitted():
+    """
+    Generate an agreement & respond w/ a pretty website that has things.
+    """
     data = dict(request.form)
     app.logger.debug(data)
 
@@ -155,7 +195,7 @@ def embed_sign():
 
     # Build URL
     url = f"{base()}api/rest/v6/agreements"
-    print(url_for("embed_submitted"))
+
     # Payload
     req = {
         "fileInfos": [{"libraryDocumentId": os.getenv("WAIVER_TEMPLATE")}],
@@ -194,15 +234,23 @@ def embed_sign():
     resp = requests.post(url, headers=headers(), json=req)
     sign_validate(resp, code=201)
 
+    # We are waiting for async processes to complete...
+    # This is NOT best practice!!!  DO NOT do this in production
+    # A better way would be to use a retry loop
+    time.sleep(3)
+
+    #  Get the new agreement id
     agreement_data = resp.json()
     agreement_id = agreement_data["id"]
 
     # Build embed URL
     embed_url = f"{base()}api/rest/v6/agreements/{agreement_id}/signingUrls"
 
+    # Get Signing URL
     embed_resp = requests.get(embed_url, headers=headers())
     sign_validate(embed_resp)
 
+    # Extract URL
     embed_data = embed_resp.json()
     signing_url = embed_data["signingUrlSetInfos"][0]["signingUrls"][0]["esignUrl"]
 
@@ -211,4 +259,4 @@ def embed_sign():
 
 @app.route("/embed/submitted", methods=["GET"])
 def embed_submitted():
-    return "<h2>Successfully submitted</h2>"
+    return f"<h2>Successfully submitted</h2>"
